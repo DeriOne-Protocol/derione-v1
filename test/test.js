92aa430361974a46c875a8e7be386d541b0e1adc
Tai <4382091+taisukemino@users.noreply.github.com>
2021-01-23 06:33:47 +0900

    remove the opyn v1 logic from the test script

--- test/test.js
+++ test/test.js
@@ -21,27 +21,16 @@ describe("DeriOneV1Main contract", function () {
       "0xEfC0eEAdC1132A12c9487d800112693bf49EcfA2";
     const hegicETHPoolV888Address =
       "0x878f15ffc8b894a1ba7647c7176e4c01f74e140b";
-    const opynExchangeV1Address = "0x39246c4f3f6592c974ebc44f80ba6dc69b817c71";
-    const opynOptionsFactoryV1Address =
-      "0xcc5d905b9c2c8c9329eb4e25dc086369d6c7777c";
-    const uniswapFactoryV1Address =
-      "0xc0a47dfe034b400b47bdad5fecda2621de6c4d95";
 
     deriOneV1Main = await DeriOneV1Main.deploy(
       hegicETHOptionV888Address,
-      hegicETHPoolV888Address,
-      opynExchangeV1Address,
-      opynOptionsFactoryV1Address,
-      uniswapFactoryV1Address
+      hegicETHPoolV888Address
     );
     console.log("deployed");
 
     const unsignedDeployTx = DeriOneV1Main.getDeployTransaction(
       hegicETHOptionV888Address,
-      hegicETHPoolV888Address,
-      opynExchangeV1Address,
-      opynOptionsFactoryV1Address,
-      uniswapFactoryV1Address
+      hegicETHPoolV888Address
     );
     let estimatedGasAmount = await provider.estimateGas(unsignedDeployTx);
     console.log("estimatedGasAmount ==>", estimatedGasAmount.toString());
