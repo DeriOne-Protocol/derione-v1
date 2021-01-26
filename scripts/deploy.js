aa57c311752a5e95865214faec4070bbb1e4cb0c
Tai <4382091+taisukemino@users.noreply.github.com>
2021-01-26 19:14:39 +0900

    instantiate the factory contract of the charm in the test and deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,11 +6,14 @@ async function deploy() {
   // get the contract to deploy
   const DeriOneV1Main = await ethers.getContractFactory("DeriOneV1Main");
   // const variables for the constructor
+  const charmV02OptionFactoryAddress =
+    "0x443ec3dc7840c3eb610a2a80068dfe3c56822e86";
   const hegicETHOptionV888Address =
     "0xEfC0eEAdC1132A12c9487d800112693bf49EcfA2";
   const hegicETHPoolV888Address = "0x878f15ffc8b894a1ba7647c7176e4c01f74e140b";
   // deploy the DeriOneV1Main contract with constructor arguments
   const deriOneV1Main = await DeriOneV1Main.deploy(
+    charmV02OptionFactoryAddress,
     hegicETHOptionV888Address,
     hegicETHPoolV888Address
   );
