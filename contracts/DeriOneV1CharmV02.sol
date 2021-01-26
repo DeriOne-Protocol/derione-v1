2281a33fd7f550ee14aa3944d2d96c0df35ac8e7
Tai <4382091+taisukemino@users.noreply.github.com>
2021-01-26 19:13:59 +0900

    instantiate the factory contract in the constructor of the charm contract

--- contracts/DeriOneV1CharmV02.sol
+++ contracts/DeriOneV1CharmV02.sol
@@ -3,5 +3,21 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/ICharmV02OptionFactory.sol";
 
-contract DeriOneV1CharmV02 is Ownable {}
+contract DeriOneV1CharmV02 is Ownable {
+    ICharmV02OptionFactory private CharmV02OptionFactoryInstance;
+
+    constructor(address _charmV02OptionFactoryAddress) public {
+        instantiateCharmV02OptionFactory(_charmV02OptionFactoryAddress);
+    }
+
+    /// @param _charmV02OptionFactoryAddress CharmV02 OptionFactoryAddress
+    function instantiateCharmV02OptionFactory(
+        address _charmV02OptionFactoryAddress
+    ) public onlyOwner {
+        CharmV02OptionFactoryInstance = ICharmV02OptionFactory(
+            _charmV02OptionFactoryAddress
+        );
+    }
+}
