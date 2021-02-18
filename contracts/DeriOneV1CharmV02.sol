2e9ba9485a9d180db71edebbaf5f3dde91c35a6a
Tai <4382091+taisukemino@users.noreply.github.com>
2021-02-18 10:56:06 +0900

    specify a type and size when you get eth option list in charm

--- contracts/DeriOneV1CharmV02.sol
+++ contracts/DeriOneV1CharmV02.sol
@@ -226,7 +226,7 @@ contract DeriOneV1CharmV02 is Ownable {
         uint256 _maxStrikeUSD,
         uint256 _sizeWEI
     ) internal view returns (DataTypes.Option[] memory) {
-        DataTypes.Option[] memory ETHCallOptionList = _getETHCallOptionList();
+        DataTypes.Option[] memory ETHCallOptionList = _getETHOptionList(_optionType, _sizeWEI);
         // _sizeWEI
 
         uint256 matchedCount;
