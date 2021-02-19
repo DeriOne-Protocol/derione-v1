2edfae70b24c36f5e943f98fe42a96c67f1fe8d8
Tai <4382091+taisukemino@users.noreply.github.com>
2021-02-19 11:26:06 +0900

    call the new hegic mathod in the main contract constructor

--- contracts/DeriOneV1Main.sol
+++ contracts/DeriOneV1Main.sol
@@ -18,11 +18,12 @@ contract DeriOneV1Main is DeriOneV1CharmV02, DeriOneV1HegicV888 {
     constructor(
         address _charmV02OptionFactoryAddress,
         address _hegicETHOptionV888Address,
-        address _hegicV888ETHPoolAddress
+        address _hegicV888ETHPoolAddress,
+        uint256 _strikesRange
     )
         public
         DeriOneV1CharmV02(_charmV02OptionFactoryAddress)
-        DeriOneV1HegicV888(_hegicETHOptionV888Address, _hegicV888ETHPoolAddress)
+        DeriOneV1HegicV888(_hegicETHOptionV888Address, _hegicV888ETHPoolAddress, _strikesRange)
     {}
 
 
