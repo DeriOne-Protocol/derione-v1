47b76df2a209d94abbbd261b519e349da41e48e0
Tai <4382091+taisukemino@users.noreply.github.com>
2021-02-19 11:26:30 +0900

    update the deploy scriot

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,11 +11,13 @@ async function deploy() {
   const hegicETHOptionV888Address =
     "0xEfC0eEAdC1132A12c9487d800112693bf49EcfA2";
   const hegicETHPoolV888Address = "0x878f15ffc8b894a1ba7647c7176e4c01f74e140b";
+  const strikesRange = 100;
   // deploy the DeriOneV1Main contract with constructor arguments
   const deriOneV1Main = await DeriOneV1Main.deploy(
     charmV02OptionFactoryAddress,
     hegicETHOptionV888Address,
-    hegicETHPoolV888Address
+    hegicETHPoolV888Address,
+    strikesRange
   );
 
   console.log("DeriOneV1Main deployed to:", deriOneV1Main.address);
