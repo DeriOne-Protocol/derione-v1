9bf592403d0dc5ab0acd2e1ccc51dcac8f137d6b
Tai <4382091+taisukemino@users.noreply.github.com>
2021-02-25 12:50:04 +0900

    empty the existing array before updating the strikes standard in hegic

--- contracts/DeriOneV1HegicV888.sol
+++ contracts/DeriOneV1HegicV888.sol
@@ -51,6 +51,7 @@ contract DeriOneV1HegicV888 is Ownable {
     }
 
     function updateStrikesStandard(uint256 _strikesRange) public onlyOwner {
+        delete strikesStandard;
         for (uint256 i = 0; i < _strikesRange; i++) {
             strikesStandard[i] = i.mul(25);
         }
