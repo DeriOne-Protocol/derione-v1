ffe4b86602004e3b5ced9febbcf83575987f0cfc
Tai <4382091+taisukemino@users.noreply.github.com>
2021-03-12 16:30:20 +0900

    feat: add the siren contract file

---
+++ contracts/DeriOneV1SirenV1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DeriOneV1SirenV1 is Ownable {}
+
+// I am not sure about their version
+
