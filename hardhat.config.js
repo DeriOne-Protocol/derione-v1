52df095c446adb7eb519842c6db899779d4f8cdc
Tai <4382091+taisukemino@users.noreply.github.com>
2021-03-29 17:28:20 +0900

    style: use backticks to extrapolate

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,17 +7,15 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url:
-          "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_API_KEY,
-        // url: "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+        // url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
         blockNumber: 12093319 // use the same block number to make subsequent runs faster with cache.
       },
       gas: "auto" // gasLimit
     },
     mainnet: {
-      url:
-        "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_API_KEY,
-      // url: "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      // url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [`0x${process.env.DEPLOYMENT_ACCOUNT_PRIVATE_KEY}`],
       gas: "auto", // gasLimit
       gasPrice: 41000000000 // make sure you check the latest gas price market in https://www.ethgasstation.info/
