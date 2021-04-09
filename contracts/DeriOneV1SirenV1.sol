33caa7dfe75f87ff8ff1a412a1fb3bbf7db37c76
Tai <4382091+taisukemino@users.noreply.github.com>
2021-04-09 12:57:12 +0900

    feat: import interface in the siren contract

--- contracts/DeriOneV1SirenV1.sol
+++ contracts/DeriOneV1SirenV1.sol
@@ -3,8 +3,10 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IMinterAmmSirenV1.sol";
+import "./interfaces/IMarketSirenV1.sol";
 
-contract DeriOneV1SirenV1 is Ownable {}
+contract DeriOneV1SirenV1 is Ownable {
 
-// I am not sure about their version
 
+}
