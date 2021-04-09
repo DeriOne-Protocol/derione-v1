b4880063b4b42d2f6fcc1a1d50e083aa172aef91
Tai <4382091+taisukemino@users.noreply.github.com>
2021-04-09 12:58:34 +0900

    feat: instantiate the minter amm

--- contracts/DeriOneV1SirenV1.sol
+++ contracts/DeriOneV1SirenV1.sol
@@ -8,5 +8,15 @@ import "./interfaces/IMarketSirenV1.sol";
 
 contract DeriOneV1SirenV1 is Ownable {
 
+    IMinterAmmSirenV1[] private minterAmmInstanceList;
 
+    function _instantiateMinterAmm(address[] memory _minterAmmAddressList)
+        private
+    {
+        for (uint256 i = 0; i < _minterAmmAddressList.length; i++) {
+            minterAmmInstanceList.push(
+                IMinterAmmSirenV1(_minterAmmAddressList[i])
+            );
+        }
+    }
 }
