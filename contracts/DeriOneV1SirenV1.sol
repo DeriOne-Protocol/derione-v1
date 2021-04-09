cb232ff4278debbe4aaa5f3f7768563c9f627acc
Tai <4382091+taisukemino@users.noreply.github.com>
2021-04-09 13:00:14 +0900

    feat: get siren market address list

--- contracts/DeriOneV1SirenV1.sol
+++ contracts/DeriOneV1SirenV1.sol
@@ -8,6 +8,7 @@ import "./interfaces/IMarketSirenV1.sol";
 
 contract DeriOneV1SirenV1 is Ownable {
 
+    address[] private marketAddressList;
     IMinterAmmSirenV1[] private minterAmmInstanceList;
 
     function _instantiateMinterAmm(address[] memory _minterAmmAddressList)
@@ -17,6 +18,17 @@ contract DeriOneV1SirenV1 is Ownable {
             minterAmmInstanceList.push(
                 IMinterAmmSirenV1(_minterAmmAddressList[i])
             );
+        }
+    }
+
+    function _getMarketAddressList(
+        IMinterAmmSirenV1[] memory _minterAmmInstanceList
+    ) private {
+        for (uint256 i = 0; i < _minterAmmInstanceList.length; i++) {
+            address[] memory markets = _minterAmmInstanceList[i].getMarkets();
+            for (uint256 i = 0; i < markets.length; i++) {
+                marketAddressList.push();
+            }
         }
     }
 }
