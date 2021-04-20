47ff4326949fd35208e7768b6b4e586e344bdce7
Tai <4382091+taisukemino@users.noreply.github.com>
2021-04-20 15:49:31 +0900

    test: rewrite a deploy script

---
+++ deploy/deploy.ts
@@ -0,0 +1,36 @@
+import { ethers } from "hardhat";
+import { CONSTRUCTOR_VALUES } from "../utils/constants";
+
+async function deploy() {
+  const DeriOneV1Main = await ethers.getContractFactory("DeriOneV1Main");
+
+  const {
+    optionFactoryAddressCharmV02,
+    ETHOptionAddressHegicV888,
+    WBTCOptionAddressHegicV888,
+    ETHPoolAddressHegicV888,
+    WBTCPoolAddressHegicV888,
+    strikesRange,
+    ammAddressListSirenV1
+  } = CONSTRUCTOR_VALUES;
+
+  // deploy the DeriOneV1Main contract with constructor arguments
+  const deriOneV1Main = await DeriOneV1Main.deploy(
+    optionFactoryAddressCharmV02,
+    ETHOptionAddressHegicV888,
+    WBTCOptionAddressHegicV888,
+    ETHPoolAddressHegicV888,
+    WBTCPoolAddressHegicV888,
+    strikesRange,
+    ammAddressListSirenV1
+  );
+
+  console.log("DeriOneV1Main deployed to:", deriOneV1Main.address);
+}
+
+deploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
