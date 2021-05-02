a8296daa71c3d2f496637a68a1c83857417d37e4
Tai <4382091+taisukemino@users.noreply.github.com>
2021-05-02 15:08:24 +0900

    feat: add the conversion library

---
+++ contracts/libraries/Conversion.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.6.0;
+
+library Conversion {
+    function stringToUint(string memory s)
+        internal
+        pure
+        returns (uint256 result)
+    {
+        bytes memory b = bytes(s);
+        uint256 i;
+        result = 0;
+        for (i = 0; i < b.length; i++) {
+            uint256 c = uint256(uint8(b[i]));
+            if (c >= 48 && c <= 57) {
+                result = result * 10 + (c - 48);
+            }
+        }
+    }
+}
